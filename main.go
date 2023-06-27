7603f1fa9affcd3c4e0da56fd86e1513cf6456e6
flowly <leverly@126.com>
2023-06-27 10:49:10 +0800

    add some examples

--- main.go
+++ main.go
@@ -3,9 +3,14 @@ package main
 import (
 	"fmt"
 	"glm/client"
+	"time"
 )
 
+var proxy *client.ChatGMLClient
+
 func main() {
+	proxy = client.NewChatGMLClient("XXX.XXX", 30*time.Second)
+
 	prompt := []client.Message{
 		{Role: "user", Content: "你好"},
 		{Role: "assistant", Content: "我是人工智能助手"},
@@ -14,11 +19,34 @@ func main() {
 		{Role: "user", Content: "你都可以做些什么事"},
 	}
 	model := "chatglm_6b"
-	proxy := client.NewChatGMLClient("xxx.xxx")
-	response, err := proxy.ChatComplete(model, 0.2, prompt)
+	response, err := proxy.Invoke(model, 0.2, prompt)
 	if err != nil {
-		fmt.Println("Error:", err)
+		fmt.Println("Invoke Error:", err)
 		return
 	}
-	fmt.Println("Response:", response)
+	fmt.Printf("Invoke Response:%s\n", (*response.Choices)[0].Content)
+}
+
+func AsyncInvoke(model string, prompt []client.Message) {
+	taskId, err := proxy.AsyncInvoke(model, 0.2, prompt)
+	if err != nil {
+		fmt.Println("Async Invoke Error:", err)
+		return
+	}
+	for true {
+		response, err := proxy.AsyncInvokeTask(model, taskId)
+		if err != nil {
+			fmt.Println("Async Invoke Task Error:", err)
+			return
+		}
+		if response.TaskStatus == client.TaskStatusSuccess {
+			fmt.Printf("AsyncInvoke Response:%s\n", (*response.Choices)[0].Content)
+			break
+		} else if response.TaskStatus == client.TaskStatusFail {
+			fmt.Println("Check Task Status Failed")
+			return
+		} else {
+			time.Sleep(200 * time.Millisecond)
+		}
+	}
 }
