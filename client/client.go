a1a1b13137d66933ba7ca75a59295d48440f5dde
flowly <leverly@126.com>
2023-06-27 10:48:54 +0800

    add asyn invoke method

--- client/client.go
+++ client/client.go
@@ -15,16 +15,16 @@ type ChatGMLClient struct {
 	client    *http.Client
 }
 
-func NewChatGMLClient(apiKey string) *ChatGMLClient {
+func NewChatGMLClient(apiKey string, timeout time.Duration) *ChatGMLClient {
 	return &ChatGMLClient{
 		apiKey:    apiKey,
 		urlPrefix: "https://open.bigmodel.cn/api/paas/v3/model-api/",
-		client:    &http.Client{},
+		client:    &http.Client{Timeout: timeout},
 	}
 }
 
-// sync method
-func (c *ChatGMLClient) ChatComplete(model string, temperature float32, prompt []Message) (*ResponseData, error) {
+// sync invoke get result
+func (c *ChatGMLClient) Invoke(model string, temperature float32, prompt []Message) (*ResponseData, error) {
 	token, err := GenerateToken(c.apiKey, 300)
 	if err != nil {
 		return nil, err
@@ -47,8 +47,87 @@ func (c *ChatGMLClient) ChatComplete(model string, temperature float32, prompt [
 	}
 	req.Header.Set("Content-Type", "application/json")
 	req.Header.Set("Authorization", token)
+	resp, err := c.client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
 
-	c.client.Timeout = 30 * time.Second
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	var response InvokeResponse
+	err = json.Unmarshal(body, &response)
+	if err != nil {
+		return nil, err
+	} else if response.Success == false {
+		return nil, fmt.Errorf("response error code:%d, msg:%s", response.Code, response.Msg)
+	}
+	return response.Data, nil
+}
+
+// return taskId for async invoke
+func (c *ChatGMLClient) AsyncInvoke(model string, temperature float32, prompt []Message) (string, error) {
+	token, err := GenerateToken(c.apiKey, 300)
+	if err != nil {
+		return "", err
+	}
+
+	request := InvokeRequest{
+		Model:       model,
+		Temperature: temperature,
+		Prompt:      prompt,
+	}
+	requestJSON, err := json.Marshal(request)
+	if err != nil {
+		return "", err
+	}
+
+	url := fmt.Sprintf("%s/%s/async-invoke", c.urlPrefix, model)
+	req, err := http.NewRequest("POST", url, strings.NewReader(string(requestJSON)))
+	if err != nil {
+		return "", err
+	}
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Authorization", token)
+
+	resp, err := c.client.Do(req)
+	if err != nil {
+		return "", err
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return "", err
+	}
+
+	var response InvokeResponse
+	err = json.Unmarshal(body, &response)
+	if err != nil {
+		return "", err
+	} else if response.Success == false {
+		return "", fmt.Errorf("response error code:%d, msg:%s", response.Code, response.Msg)
+	}
+	return response.Data.TaskID, nil
+}
+
+// get async invoke task result
+func (c *ChatGMLClient) AsyncInvokeTask(model, taskId string) (*ResponseData, error) {
+	token, err := GenerateToken(c.apiKey, 300)
+	if err != nil {
+		return nil, err
+	}
+
+	url := fmt.Sprintf("%s/%s/async-invoke/%s", c.urlPrefix, model, taskId)
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return nil, err
+	}
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Authorization", token)
 	resp, err := c.client.Do(req)
 	if err != nil {
 		return nil, err
